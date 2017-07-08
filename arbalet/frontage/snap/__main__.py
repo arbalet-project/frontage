84db04e028aecd5986f5cd33a0920f6ea13db3bb
Yoan Mollard <yoan.mollard@inria.fr>
2017-07-08 21:32:46 +0200

    Read arguments

--- arbalet/frontage/snap/__main__.py
+++ arbalet/frontage/snap/__main__.py
@@ -6,6 +6,15 @@ parser = ArgumentParser(description='Server for the Snap visual programming lang
                                     'Open the online Snap environment in your web browser: http://snap.berkeley.edu/run '
                                     'then select File > Import and choose the file "xml/arbalet.xml" file on your local disk')
 
+parser.add_argument('-w', '--hardware',
+                    action='store_true',
+                    help='The server must connect to Arbalet hardware ')
+
+parser.add_argument('-n', '--no-simulation',
+                    dest='simulation',
+                    action='store_false',
+                    help='Disable the LED/touch device simulation that is automatically started otherwise')
+
 args = parser.parse_args()
 
-SnapServer(33450).run()
+SnapServer(33450, args.hardware, args.simulation).run()
