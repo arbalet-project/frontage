7997ea576326ca4430828c73b1d3ecba292ae8ce
abdel140 <45460785+abdel140@users.noreply.github.com>
2019-02-08 12:44:59 +0100

    Add the workshop page with basic blocks

---
+++ snap/Arbalet_Home_page/workshop.js
@@ -0,0 +1,8 @@
+
+
+Blockly.JavaScript['text_length'] = function(block) {
+    // String or array length.
+    var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+        Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+    return [argument0 + '.length', Blockly.JavaScript.ORDER_MEMBER];
+};
